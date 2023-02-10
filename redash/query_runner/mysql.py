bdcf9609a5646d5166ec0a3fe923662b7d9831c1
樊国睿 <guorui.fan@baishan.com>
2023-02-10 14:11:47 +0800

    feat: support databend and mysql client upgrade to a 2.1.1 version

--- redash/query_runner/mysql.py
+++ redash/query_runner/mysql.py
@@ -131,7 +131,7 @@ class Mysql(BaseSQLQueryRunner):
                col.table_name as table_name,
                col.column_name as column_name
         FROM `information_schema`.`columns` col
-        WHERE col.table_schema NOT IN ('information_schema', 'performance_schema', 'mysql', 'sys');
+        WHERE col.table_schema NOT IN ('information_schema', 'performance_schema', 'mysql', 'sys','system');
         """
 
         results, error = self.run_query(query, None)
